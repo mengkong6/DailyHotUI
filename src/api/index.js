298150d3514d5c3d09ca88523238aa52c3936238
lixin <727338622@qq.com>
2023-03-31 16:01:29 +0800

    first commit

---
+++ src/api/index.js
@@ -0,0 +1,15 @@
+import axios from "@/api/request";
+
+
+/**
+ * 获取热榜分类数据
+ * @param {string} type 热榜分类名称
+ * @param {boolean} isNew 是否拉取最新数据
+ * @returns
+ */
+export const getHotLists = (type, isNew) => {
+  return axios({
+    method: "GET",
+    url: `?type=${type}`,
+  });
+};
